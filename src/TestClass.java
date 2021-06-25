078580854ec6ddc05acbff55e139271fb855cb3a
Kurush Yusufiy <narusufiy@gmail.com>
2021-06-24 20:46:24 -0400

    Added the files

---
+++ src/TestClass.java
@@ -0,0 +1,24 @@
+public class TestClass {
+    public static void main(String[] args) {
+        binarySearch(new int[]{1, 2, 3}, 0, 2, 2);
+
+    }
+
+    public static void binarySearch(int arr[], int first, int last, int key) {
+        int mid = (first + last) / 2;
+        while (first <= last) {
+            if (arr[mid] < key) {
+                first = mid + 1;
+            } else if (arr[mid] == key) {
+                System.out.println("Element is found at index: " + mid);
+                break;
+            } else {
+                last = mid - 1;
+            }
+            mid = (first + last) / 2;
+        }
+        if (first > last) {
+            System.out.println("Element is not found!");
+        }
+    }
+}
